61ff9de0b3651ce367424b081dfb6588727d0282
subkeezy <102140976+subkeezy@users.noreply.github.com>
2024-01-24 16:34:10 +0700

    Add files via upload

---
+++ 2.2.14/src/components/App/App.js
@@ -0,0 +1,13 @@
+import MovieList from '../MovieList/MovieList';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="movie">
+      <MovieList />
+    </div>
+  );
+};
+
+export default App;
